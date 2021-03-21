74eca24a29cacc0c058403603c55e1bdb47e85ae
Sankalp <2020a1t109@mietjammu.in>
2021-03-21 23:42:54 +0530

    Maximum of n numbers using for loop file added

---
+++ 12_MaxNumForLoop.c
@@ -0,0 +1,27 @@
+//Author- Sankalp Bajaj
+//21/March/2020
+//Maximun of N numbers Using For Loop
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int i,num,n,large=0; // It will set the number of values to be entered by user based on their choice
+	printf("How many numbers:");
+	scanf("%d",&n);
+
+	for(i=0; i<n; i++) // It will scan the numbers entered by user upto the limit that has been set
+	{
+		printf("\nEnter number %d: ",i+1);
+		scanf("%d",&num);
+		if(num>large)
+		large=num;
+	}
+
+	printf("\nThe largest number is %d",large); // It will give the largest number as the output
+
+	getch();
+
+	return 0;	
+}
