fc3a6980332996d0b84beb774633321bcd78f721
Sankalp <2020a1t109@mietjammu.in>
2021-03-30 02:10:08 +0530

    Max Element and Index in Array File Added

---
+++ 16_MaxElementIndexArr.c
@@ -0,0 +1,27 @@
+//Author- Sankalp Bajaj
+//30/March/2020
+//To Find Max Element and Index in Array
+
+
+#include<stdio.h>
+int main(){
+	int i;
+	float arr[5];
+
+	printf("Please enter five numbers:\n ");
+
+	for (i = 0; i < 5; ++i) //Using for loop.
+	{
+    scanf("%f", &arr[i]);//Scanning the user entered values.
+	}
+
+		for (i = 1; i < 5; ++i)
+		{
+			//Determing the largest value.
+    	if (arr[0] < arr[i])
+        	arr[0] = arr[i];
+		}
+	printf("Largest element = %.2f", arr[0]);//Printing the largest value.
+
+	return 0;
+}
