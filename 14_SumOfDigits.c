eb074827adc035bc6652d6e7c41506fa1f9e9225
Sankalp <2020a1t109@mietjammu.in>
2021-03-30 02:00:32 +0530

    Sum of Digits File Added

---
+++ 14_SumOfDigits.c
@@ -0,0 +1,27 @@
+//Author- Sankalp Bajaj
+//29/March/2020
+//Sum of Digits of a Number Using While Loop
+
+
+#include<stdio.h>
+int main()
+{
+	int Number, Reminder , Sum=0;
+
+	printf("Please Enter any number\n");
+	scanf("%d", &Number);// This willl read the number entered by user
+
+	while(Number > 0)//Using while Loop
+	{
+	    //This is the formula to calculate sum of digits
+	    Reminder = Number % 10;
+	    Sum = Sum+Reminder;
+	    Number = Number/ 10;
+	}
+
+    printf("Sum of the digits of given number = %d\n", Sum);
+
+    return 0;
+}
+
+	
