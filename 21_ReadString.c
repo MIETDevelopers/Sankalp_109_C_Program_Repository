8650f04c5a4c7ddddfb18e68495664ce785646f3
Sankalp <2020a1t109@mietjammu.in>
2021-03-31 21:31:58 +0530

    String file added

---
+++ 21_ReadString.c
@@ -0,0 +1,13 @@
+//Author- Sankalp Bajaj
+//31/March/2021
+//program to read string and give it as output
+
+#include <stdio.h>
+int main()
+{
+    char name[20];
+    printf("Enter name: \n");
+    scanf("%s", &name); //This will read the name.
+    printf("Your name is %s.", name); //This will show (print) the name.
+    return 0;
+}
