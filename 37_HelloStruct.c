9844efe3b45668e4b9df7878aa6e4d62741482cc
Sankalp <2020a1t109@mietjammu.in>
2021-03-31 23:57:02 +0530

    Program of Hello World that would return to a structure data type

---
+++ 37_HelloStruct.c
@@ -0,0 +1,13 @@
+//Author-Sankalp Bajaj
+//31/March/2021
+//Program of Hello World that would return to a structure data type
+
+#include<stdio.h> //Including standard input ouput header files from library.
+struct s {
+	char text[50];
+	};
+int main(){
+	struct s a[10];	
+	printf("Hello Buddy!!");//Here we entered the texted to be printed.
+	return 's';
+} 
