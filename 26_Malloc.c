ad8410d8c2b2aedab8c54037f3fcdb8fa86cc2ea
Sankalp <2020a1t109@mietjammu.in>
2021-03-31 18:42:05 +0530

    Malloc function file added

---
+++ 26_Malloc.c
@@ -0,0 +1,15 @@
+//Author- Sankalp Bajaj
+//31/March/2021
+//To demonstrate use of malloc function
+
+
+#include <stdio.h>
+#include <stdlib.h> //Using standard library header file.
+	int main(){
+	int *ptr;
+		ptr = malloc(15 * sizeof(*ptr)); //A block of 15 integers.
+    	if (ptr != NULL) {
+        *(ptr + 5) = 298; //Assign 298 to sixth integer.
+    printf("Value of the 6th integer is %d",*(ptr + 5));
+    }
+}
